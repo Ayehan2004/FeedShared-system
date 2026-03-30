39a13e0805f768199a9e5c76b0d844bedb7dbfb9
Ayehan2004 <maxayeshan992@gmail.com>
2026-03-30 23:10:41 +0530

    database created

--- src/app/app.py
+++ src/app/app.py
@@ -1,33 +1,12 @@
 from fastapi import FastAPI,HTTPException
+from .schemas import PostCreate, PostResponse
+from .db import create_db_and_tables, get_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
-text_posts = {
-    "1": {"title": "First Post", "content": "This is the first post."},
-    "2": {"title": "Second Post", "content": "This is the second post."},
-    "3": {"title": "Third Post", "content": "This is the third post."},
-    "4": {"title": "Fourth Post", "content": "This is the fourth post."},
-    "5": {"title": "Fifth Post", "content": "This is the fifth post."},
-    "6": {"title": "Sixth Post", "content": "This is the sixth post."},
-    "7": {"title": "Seventh Post", "content": "This is the seventh post."},
-    "8": {"title": "Eighth Post", "content": "This is the eighth post."},
-    "9": {"title": "Ninth Post", "content": "This is the ninth post."},
-    "10": {"title": "Tenth Post", "content": "This is the tenth post."}
-}
-
-@app.get("/post")
-def get_all_posts(limit : int = None):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
-
-@app.get("/post/{id}")
-def get_post(id: str):
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return text_posts.get(id)
-
-
-@app.post("/post")
-def create_post():
-    pass 
+app = FastAPI(lifespan=lifespan)
